1259ae727d286f581622d37751908213cb659e28
Peter Jenkins <wringtonpete@gmail.com>
2020-11-12 12:32:02 +0000

    AAAAAAA

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('bookshop-db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
-server.listen(process.env.PORT, () => {
+server.listen(process.env.PORT || 80, () => {
   console.log('JSON Server is running')
 })
